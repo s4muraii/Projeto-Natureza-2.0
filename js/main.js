272139ea817d47374774cacca9f27fa03099bf87
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-09-19 13:25:28 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
-console.log("olá mundo!!!")
+const imagens = document.querySelectorAll('.js-list li');
+const textos = document.querySelectorAll('.js-content section');
 
+
+function ativarConteudo (indice){
+    textos[indice].classList.add('active')
+}
