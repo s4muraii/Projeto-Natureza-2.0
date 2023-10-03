8c9db89bf3d7b8bdc47fbec4baa53f66a24c247d
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-10-03 10:45:55 -0300

    upd\te

---
+++ js/modules/accordion.js
@@ -0,0 +1,13 @@
+export default function accordionInit(){
+    const faqs = document.querySelectorAll(".faq");
+    faqs.forEach(faq => {
+        faq.addEventListener("click", () => {
+            if (faq.classList.contains("active")) {
+                faq.classList.remove("active");
+            } else {
+                faqs.forEach(otherFaq => otherFaq.classList.remove("active"));
+                faq.classList.add("active");
+            }
+        });
+    });
+}
